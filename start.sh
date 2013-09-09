b686fe359c94a0542ab98015902be8a8630265a0
Noah Isaacson <noah@nisaacson.com>
2013-09-09 16:11:26 -0600

    Extract port from dd-config.json
    Install json-extract if needed in start.sh and ready.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,14 @@
+function install_json_extract {
+  command -v json-extract > /dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    return
+  fi
+  echo '-----> installing json-extract now'
+  sudo npm install -g --silent json-extract
+}
+
+install_json_extract
+
+PORT=$(json-extract --file ./dd-config.json --key ports.0)
+export PORT=$PORT
 ./start-web.js
