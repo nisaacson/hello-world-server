34e25ef5bf2e557b08402dd8b87001f6701a1304
Noah Isaacson <noah@nisaacson.com>
2013-10-03 12:46:30 -0600

    Use default port 3000 in ready.sh.

--- ready.sh
+++ ready.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 
-install_json_extract() {
-  command -v json-extract > /dev/null 2>&1
-  if [ $? -eq 0 ]; then
-    return
-  fi
-  echo '-----> installing json-extract now'
-  npm install -g json-extract
-}
-
-install_json_extract
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-CONFIG_PATH="$DIR/dd-config.json"
-echo "reading config at path: $CONFIG_PATH"
-PORT=$(json-extract --file $CONFIG_PATH --key ports.0)
+PORT=${PORT:-3000}
 URL=localhost:$PORT
 
 while true
